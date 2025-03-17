3df7aabe81494a36310f39fcfdb63a53379c2d9a
n1tr0xs <niko4pok@gmail.com>
2025-03-17 19:02:57 +0300

    source files moved to src"

--- constants.py
+++
@@ -1,5 +0,0 @@
-WINDOW_TITLE = ""
-DB_FILE_PATH = 'data.db'
-RECEPTOR_FILE_PATH = 'ReceptorSearchA_2024.csv'
-COMPOUND_FILE_PATH = 'compoundsnamesA_2024.csv'
-LIGAND_FILE_PATH = 'ligandReceptorsA_2024.csv'
